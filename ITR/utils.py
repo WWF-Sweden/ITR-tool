dc1920558f4d785239dc277bfb768f7096b79a08
mountainrambler <mountainrambler@gmail.com>
2024-06-07 12:18:13 +0200

    Avoid FutureWarnings

--- ITR/utils.py
+++ ITR/utils.py
@@ -192,7 +192,7 @@ def dataframe_to_portfolio(df_portfolio: pd.DataFrame) -> List[PortfolioCompany]
     :return: A list of portfolio companies
     """
     df_portfolio[ColumnsConfig.ENGAGEMENT_TARGET] = (
-        df_portfolio[ColumnsConfig.ENGAGEMENT_TARGET].fillna(False).infer_objects().astype("bool")
+        df_portfolio[ColumnsConfig.ENGAGEMENT_TARGET].astype("bool").fillna(False)
     )
 
     if 'user_fields' in df_portfolio:
