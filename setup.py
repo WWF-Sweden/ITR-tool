c3c91da63c844e9fa093c59d9fdeb7b39fd613b7
mountainrambler <mountainrambler@gmail.com>
2025-11-26 17:04:28 +0100

    poetry.lock

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ package_data = {"": ["*"], "ITR": ["inputs/*"]}
 install_requires = [
     "openpyxl==3.1.2",
     "pandas==2.2.2",
-    "pydantic==1.10.14",
+    "pydantic>=2.0,<3.0",
     "requests==2.32.3",
     "six>=1.16.0",
     "xlrd==2.0.1",
